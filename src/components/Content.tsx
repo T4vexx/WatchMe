5aa277bd46833f0c98680938c47f4979eff98090
Otávio Augusto <tavinteixeira@hotmail.com>
2022-03-21 22:42:03 -0300

    After Performance check

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,4 @@
+import { List, ListRowRenderer, AutoSizer } from 'react-virtualized'
 import { MovieCard } from "./MovieCard";
 
 interface ContentProps {
@@ -20,6 +21,14 @@ interface ContentProps {
 }
 
 export function Content({ selectedGenre, movies }: ContentProps) {
+  const rowRenderer: ListRowRenderer = ({index, key, style}) => {
+    return (
+      <div key={key} style={style}>
+        <MovieCard key={movies[index].imdbID} title={movies[index].Title} poster={movies[index].Poster} runtime={movies[index].Runtime} rating={movies[index].Ratings[0].Value} />
+      </div>
+    )
+  }
+
   return (
     <div className="container">
       <header>
@@ -27,10 +36,15 @@ export function Content({ selectedGenre, movies }: ContentProps) {
       </header>
 
       <main>
-        <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
+        <div className="movies-list"> 
+        <List 
+          height={770}
+          rowHeight={340}
+          width={840}
+          overscanRowCount={5}
+          rowCount={movies.length}
+          rowRenderer={rowRenderer}  
+        />
         </div>
       </main>
     </div>
